fddf0e351dfcaeb392a157f8afe3e12610c82f6f
lorvynskyladouceur-creator <lorvynskyladouceur@gmail.com>
2025-08-09 19:01:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "YOUR SESSION_ID",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~HtZB1ARK#avfQdQS8HPqMfwmmDEM-fPZjoDhXb2--kinU_LJZdZo",
 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
